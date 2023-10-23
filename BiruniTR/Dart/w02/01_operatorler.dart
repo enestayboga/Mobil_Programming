5de557f35ce8addd044a7b4f6b09d127825e1b77
Enes Tayboğa <96184712+enestayboga@users.noreply.github.com>
2023-10-23 17:02:33 +0300

    u

---
+++ BiruniTR/Dart/w02/01_operatorler.dart
@@ -0,0 +1,43 @@
+
+
+void main(List<String> args) {
+  
+    //A-binary operatörler/ikili op.
+    //01-Aritmetik Op. +, -, *, /, %     
+    //=> sol_operand operator sag_operand => 1 + 2   
+    //=> geriye sayısal değer döndür
+    
+  double a = 1 / 9+ 8 * 9 - 9 + 5 - 15 % 3;
+
+      //02-Assignment/Atama Operatorleri => 
+      //=, +=, -=, *=, /=,  %=  
+      //=> sol_operand operator sag_operand 
+      //=> int a; a = 2; // atama yapar
+
+  double a2 = 5;
+      a2 *= 5;    //a2 = a2 * 5;
+      a2 += 1;    //a2 = a2 + 1; 
+
+        //03-Relational/İlişkisel op
+        //=>  <, >, <=, >=, !=, ==, 
+        //=> sol_operand operator sag_operand 
+        //=> 1 + 2    => geriye sayısal boolean döndür
+
+  bool is0k1 = 1 !=1;
+
+          //04-Logical/Mantıksal op
+          //=>  &&, ||
+          //=> sol_operand operator sag_operand 
+          //=> 1 + 2    => geriye sayısal boolean döndür
+
+   bool is0k2 = ((1 < 5) && (8 == 9)) || (9 >= -9);
+
+  //-------------------------------------------------------
+
+    
+
+
+
+
+
+}
