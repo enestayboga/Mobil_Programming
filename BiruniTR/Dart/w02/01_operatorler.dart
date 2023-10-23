e49afbad2453c5a5d6cee5b24f81ce0191e09d54
enestayboga <enestayboga44@gmail.com>
2023-10-24 00:08:35 +0300

    u

--- BiruniTR/Dart/w02/01_operatorler.dart
+++ BiruniTR/Dart/w02/01_operatorler.dart
@@ -1,43 +1,66 @@
-
-
 void main(List<String> args) {
-  
-    //A-binary operatörler/ikili op.
-    //01-Aritmetik Op. +, -, *, /, %     
-    //=> sol_operand operator sag_operand => 1 + 2   
-    //=> geriye sayısal değer döndür
-    
-  double a = 1 / 9+ 8 * 9 - 9 + 5 - 15 % 3;
+  //Operatörler 3e ayrılırlar. Binary, unary ve ternary
 
-      //02-Assignment/Atama Operatorleri => 
-      //=, +=, -=, *=, /=,  %=  
-      //=> sol_operand operator sag_operand 
-      //=> int a; a = 2; // atama yapar
+  // A-binary operatörler/ikili op.
+
+  // 01-Aritmetik Op. +, -, *, /, %
+  // => sol_operand - operator - sag_operand => 1 + 2
+  // => geriye sayısal değer döndür
+
+  double a = 1 / 9 + 8 * 9 - 9 + 5 - 15 % 3;
+
+  // 02-Assignment/Atama Operatorleri =>
+  // =, +=, -=, *=, /=,  %=
+  // => sol_operand operator sag_operand
+  // => int a; a = 2; // atama yapar
 
   double a2 = 5;
-      a2 *= 5;    //a2 = a2 * 5;
-      a2 += 1;    //a2 = a2 + 1; 
+  a2 *= 5; //a2 = a2 * 5;
+  a2 += 1; //a2 = a2 + 1;
 
-        //03-Relational/İlişkisel op
-        //=>  <, >, <=, >=, !=, ==, 
-        //=> sol_operand operator sag_operand 
-        //=> 1 + 2    => geriye sayısal boolean döndür
+  // 03-Relational/İlişkisel operatorler
+  // =>  <, >, <=, >=, !=, ==,
+  // => sol_operand operator sag_operand
+  // => 1 + 2    => geriye sayısal boolean döndür (çok önemli)
 
-  bool is0k1 = 1 !=1;
+  bool isOk1 = 1 != 1; // isOk1 => is Okey 1
 
-          //04-Logical/Mantıksal op
-          //=>  &&, ||
-          //=> sol_operand operator sag_operand 
-          //=> 1 + 2    => geriye sayısal boolean döndür
+  // 04-Logical/Mantıksal operatorler
+  // =>  &&, ||
+  // => sol_operand operator sag_operand
+  // => 1 + 2    => geriye sayısal boolean döndür
 
-   bool is0k2 = ((1 < 5) && (8 == 9)) || (9 >= -9);
+  bool isOk2 = ((1 < 5) && (8 == 9)) || (9 >= -9);
 
   //-------------------------------------------------------
 
-    
+  // B-unary operatorler, tekli
 
+  // 04-Logical/Mantıksal operatorleri
+  // =>  ! => operator sag_operand (Tek bir tarafda operand var)
+  // => !(true/false)
+  // => geriye sayısal boolean döndür
 
+  bool isOk3 = !(((1 < 5) && (8 == 9)) || (9 >= -9));
 
+  // (!) -> operator görevi görür
+  // (((1 < 5) && (8 == 9)) || (9 >= -9)); ise sag_operand
 
+  // 05-Increment/Decrement/ Arttırma/Azaltma operatorleri
+  // =>    ++, --
+  // => a++, --a,
+  // => geriye genelde sayısal değer döner
 
+  double a3 = 5;
+  a3++;
+  a3 = ++a3 + --a2 * (a--);
+
+  //-------------------------------------------------------
+
+  // C-ternary operatorler, 3 lü
+
+  String cinsiyet = "Bayan";
+  double yasSinir = cinsiyet == "Erkek" ? 1 : 0;
+
+  // (==) 1. operatör, (?) 2. operatör, (:) 3. operatör
 }
--- BiruniTR/Dart/w02/02_donguler.dart
+++ BiruniTR/Dart/w02/02_donguler.dart
@@ -1,3 +1,82 @@
 void main(List<String> args) {
-  
+  // A-Iteratif döngüler
+
+  // loops => döngüler
+
+  for (var i = 1; i <= 15; i++) {
+    print(i);
+  }
+  print("-------------------------------");
+
+  int sayac = 1;
+  while (sayac <= 15) {
+    print(sayac);
+    sayac++;
+  }
+  print("-------------------------------");
+
+  do {
+    print(sayac);
+    sayac++;
+  } while (sayac <= 15);
+
+  print("-------------------------------");
+
+  // infinite loops => sonsuz döngüler
+
+  /*
+
+  for (;;) {
+    print("İşlem");
+    if (true) ;
+    break; döngüden tamamen çıkar.
+    continue; alt satırı çalıştırmaz, döngü başıan döner.
+  }
+
+   */
+
+  //------------------------------------
+
+  /*
+
+  while (true) {
+    break;
+    continue;
+  }
+
+  */
+
+  //------------------------------------
+
+  // recursive döngüler => özyinelemeli fonksiyonlar
+  // bu fonksiyonlar döngü görevi görürler.
+
+  //Fibonacci => 0 1 1 2 3 5 8 13 21 ...
+  int Fibonacci(int index) {
+    if (index == 0 || index < 0) return 0;
+    if (index == 1 || index == 2) return 1;
+
+    return Fibonacci(index - 1) + Fibonacci(index - 2);
+  }
+
+  for (var i = 0; i <= 5; i++) {
+    print(Fibonacci(i));
+  }
+
+  // 1 den n e kadar olan sayıların toplamı:
+
+  int Toplam(int n) {
+    if (n == 0) return 0;
+    var toplama = n + Toplam(n - 1);
+    return toplama;
+  }
+
+  print(Toplam(10));
+
+  //for in dongusu
+
+  List<int> list1 = [1, 2, 3];
+  for (var element in list1) {
+    print(element);
+  }
 }
